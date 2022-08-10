3eda7dc2a42570fa84318879c9571c5aeedb0ddf
Sanyi81 <skyharts2002@gmail.com>
2022-08-10 20:05:45 +0300

    add resolve6

--- lesson6/resolve6/main.js
+++ lesson6/resolve6/main.js
@@ -8,9 +8,9 @@
 // let str1 = 'hello world';
 // let str2 = 'lorem ipsum';
 // let str3 = 'javascript is cool';
-// console.log(str1);
-// console.log(str2);
-// console.log(str3);
+// console.log(str1.length);
+// console.log(str2.length);
+// console.log(str3.length);
 //
 // let strUp1 = str1;
 // let strUp2 = str2;
